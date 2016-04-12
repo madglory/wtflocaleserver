91dcd5794ea1224c92b14f36fa662109b82353a4
Brian Corrigan <bcorrigan78@gmail.com>
2016-04-12 11:10:54 -0400

    Updates language matcher to use BCP 47

--- main.go
+++ main.go
@@ -7,8 +7,91 @@ import (
 	"net/http"
 	"os"
 
-	"github.com/swhite24/go-locale/locale"
+	"golang.org/x/text/language"
 )
+
+// This is a list of all languages that are supported by the service.
+var matcher = language.NewMatcher([]language.Tag{
+	language.AmericanEnglish,
+	language.Afrikaans,
+	language.Amharic,
+	language.Arabic,
+	language.ModernStandardArabic,
+	language.Azerbaijani,
+	language.Bulgarian,
+	language.Bengali,
+	language.Catalan,
+	language.Czech,
+	language.Danish,
+	language.German,
+	language.Greek,
+	language.English,
+	language.BritishEnglish,
+	language.Spanish,
+	language.EuropeanSpanish,
+	language.LatinAmericanSpanish,
+	language.Estonian,
+	language.Persian,
+	language.Finnish,
+	language.Filipino,
+	language.French,
+	language.CanadianFrench,
+	language.Gujarati,
+	language.Hebrew,
+	language.Hindi,
+	language.Croatian,
+	language.Hungarian,
+	language.Armenian,
+	language.Indonesian,
+	language.Icelandic,
+	language.Italian,
+	language.Japanese,
+	language.Georgian,
+	language.Kazakh,
+	language.Khmer,
+	language.Kannada,
+	language.Korean,
+	language.Kirghiz,
+	language.Lao,
+	language.Lithuanian,
+	language.Latvian,
+	language.Macedonian,
+	language.Malayalam,
+	language.Mongolian,
+	language.Marathi,
+	language.Malay,
+	language.Burmese,
+	language.Nepali,
+	language.Dutch,
+	language.Norwegian,
+	language.Punjabi,
+	language.Polish,
+	language.Portuguese,
+	language.BrazilianPortuguese,
+	language.EuropeanPortuguese,
+	language.Romanian,
+	language.Russian,
+	language.Sinhala,
+	language.Slovak,
+	language.Slovenian,
+	language.Albanian,
+	language.Serbian,
+	language.SerbianLatin,
+	language.Swedish,
+	language.Swahili,
+	language.Tamil,
+	language.Telugu,
+	language.Thai,
+	language.Turkish,
+	language.Ukrainian,
+	language.Urdu,
+	language.Uzbek,
+	language.Vietnamese,
+	language.Chinese,
+	language.SimplifiedChinese,
+	language.TraditionalChinese,
+	language.Zulu,
+})
 
 type geoIP struct {
 	Language      string `json:"language"`
@@ -26,11 +109,12 @@ type geoIP struct {
 }
 
 func geoIPFromRequest(r *http.Request) (g geoIP) {
-	ls := locale.Read(r.Header.Get("Accept-Language"))
-	l := ls.Best()
+	t, _, _ := language.ParseAcceptLanguage(r.Header.Get("Accept-Language"))
+	// We ignore the error: the default language will be selected for t == nil.
+	tag, _, _ := matcher.Match(t...)
 
 	return geoIP{
-		l.Language,
+		tag.String(),
 		r.Header.Get("X-GEO-LATITUDE"),
 		r.Header.Get("X-GEO-LONGITUDE"),
 		r.Header.Get("X-GEO-CITY"),
